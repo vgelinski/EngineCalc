26fab879486e3ad3aeb718eae6d30c4039aa6940
Valentin <gelinski.v@gmail.com>
2018-01-31 02:00:18 +0200

    Added integral (not implemented yet)

---
+++ src/math/integral.cc
@@ -0,0 +1,13 @@
+#include "integral.h"
+
+using namespace std;
+
+shared_ptr<Function> IntegralCalculator::integrate(shared_ptr<Function> f,
+        double start, double end, const string &param, int riemannSumCount) {
+    return make_shared<SingleThreadIntegral>();
+}
+
+double IntegralCalculator::SingleThreadIntegral::
+        value(const map<string, double> &params) const {
+    return 0;
+}
---
+++ src/math/integral.h
@@ -0,0 +1,30 @@
+#ifndef INTEGRAL_H
+#define INTEGRAL_G
+
+#include <memory>
+
+#include "function.h"
+
+namespace std{
+
+class IntegralCalculator {
+
+public:
+
+    static shared_ptr<Function> integrate(shared_ptr<Function> f,
+                                          double start, double end,
+                                          const string &param,
+                                          int riemannSumCount = 1000);
+
+private:
+    class SingleThreadIntegral : public Function {
+
+    private:
+
+    public:
+
+        virtual double value(const map<string, double> &params) const override;
+    };
+};
+}
+#endif
