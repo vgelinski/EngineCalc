5f42a7629cbd0c66bbe4b813834a4fe1f7e0bd17
Valentin Gelinski <vgelinski@hopper.com>
2019-05-21 18:27:32 +0300

    Identity test

---
+++ src/test/math/identity_test.cc
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+
+#include "../../math/identity.h"
+
+using namespace std;
+
+TEST (IdentityTest, valueTest) {
+    const Identity x("x");
+    map<string, double> params;
+    params["x"] = 2.0;
+    ASSERT_EQ(x.value(params), 2);
+}
