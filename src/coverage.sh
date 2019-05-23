36a8180045e3daeaab3e62b6c3429c16eb3c69fe
Valentin Gelinski <vgelinski@hopper.com>
2019-05-23 14:33:23 +0300

    put all coverage files in coverage dir

--- src/coverage.sh
+++ src/coverage.sh
@@ -1,7 +1,11 @@
-LCOV_OUT=app.info
+COV_DIR=coverage
+LCOV_OUT=$COV_DIR/app.info
 
 make test
 ./bin/test
 gcov -b -c test_runner.cc
-lcov --directory . --capture --output-file $LCOV_OUT
-genhtml $LCOV_OUT --output-directory coverage
+mkdir -p $COV_DIR
+mv *.gcno $COV_DIR/
+mv *.gcda $COV_DIR/
+lcov --directory $COV_DIR --capture --output-file $LCOV_OUT
+genhtml $LCOV_OUT --output-directory $COV_DIR
