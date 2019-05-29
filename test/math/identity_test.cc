2c86f13010095c7e706db4432b324f821c6bc2a8
Valentin Gelinski <vgelinski@hopper.com>
2019-05-29 15:07:14 +0300

    Improve testing

--- test/math/identity_test.cc
+++ test/math/identity_test.cc
@@ -7,7 +7,10 @@ using namespace engc::math;
 
 TEST (IdentityTest, valueTest) {
     const Identity x("x");
+    const Identity y("y");
     map<string, double> params;
     params["x"] = 2.0;
+    params["y"] = 3.0;
     ASSERT_EQ(x.value(params), 2);
+    ASSERT_EQ(y.value(params), 3);
 }
