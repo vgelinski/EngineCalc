1fc500d9afb59994e06d4d13d90dff3aa94607eb
Valentin Gelinski <vgelinski@hopper.com>
2019-06-11 14:06:23 +0300

    MultipleUnit

--- src/physics/units.cc
+++ src/physics/units.cc
@@ -3,9 +3,20 @@
 using namespace engc::physics;
 using namespace std;
 
-SimpleUnit::SimpleUnit(const SimpleUnitType type, const string &unit):
-        type(type), unit(unit) {}
+SimpleUnit::SimpleUnit(const SimpleUnitType type, const string &unit)
+        : type(type), unit(unit) {}
 
 SimpleUnit::~SimpleUnit() {}
 
 string SimpleUnit::toString() const {return unit;}
+
+MultipleUnit::MultipleUnit(
+        const long double &multiplier,
+        const SimpleUnit &unit,
+        const string &name)
+        : multiplier(multiplier), unit(unit), name(name) {}
+
+MultipleUnit::~MultipleUnit() {}
+
+string MultipleUnit::toString() const {return name;}
+
--- src/physics/units.h
+++ src/physics/units.h
@@ -11,13 +11,31 @@ enum class SimpleUnitType {
 
 class SimpleUnit {
 private:
-    const SimpleUnitType type;
+    const SimpleUnitType &type;
     const std::string unit;
 
     SimpleUnit(const SimpleUnitType type, const std::string &unit);
 
 public:
     virtual ~SimpleUnit();
+
+    virtual std::string toString() const;
+};
+
+class MultipleUnit {
+private:
+    const long double multiplier;
+    const SimpleUnit &unit;
+    const std::string name;
+
+    MultipleUnit(
+            const long double &multiplier,
+            const SimpleUnit &unit,
+            const std::string &name
+    );
+
+public:
+    virtual ~MultipleUnit();
 
     virtual std::string toString() const;
 };
