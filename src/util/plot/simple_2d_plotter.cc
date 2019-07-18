98bda01ff5f134737f52f6785114690b8544b6fb
Valentin Gelinski <vgelinski@hopper.com>
2019-07-18 17:05:06 +0300

    Implemented simple 2d plotter

--- src/util/plot/simple_2d_plotter.cc
+++ src/util/plot/simple_2d_plotter.cc
@@ -1,9 +1,14 @@
 #include "simple_2d_plotter.h"
 
+#include <numeric>
+
+#include "../files/file_io.h"
+
 #define DC DataCalculator2D
 #define S2DP Simple2DPlotter
 
 using namespace std;
+using namespace engc::physics;
 using namespace engc::util;
 
 S2DP::S2DP(
@@ -19,6 +24,26 @@ S2DP::S2DP(
 S2DP::~S2DP() {}
 
 void S2DP::plot() {
+    string plotContent = "";
+    plotContent += string(std::accumulate(
+            lineNames.begin(),
+            lineNames.end(),
+            plotContent,
+            [this](const string& a, const string& b) -> string {
+                return a + (a.length() > 0 ? separator + "\t" : "") + b;
+            }
+     ));
+    for (auto row : dataCalc->calculateData()) {
+        plotContent += std::accumulate(
+                row.begin(),
+                row.end(),
+                string(),
+                [this](const string& a, const shared_ptr<Value>& b) -> string {
+                    return a + (a.length() > 0 ? separator + "\t" : "") + to_string(b->value);
+                }
+        );
+    }
+    FileIO::writeToFile(filename, plotContent);
 }
 
 
