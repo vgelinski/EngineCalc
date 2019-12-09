92916913e4c940de931d6534b6bcef3557581c6c
Valentin Gelinski <vgelinski@hopper.com>
2019-12-09 13:04:59 +0200

    Restructure main file to support different calculations

--- src/main.cc
+++ src/main.cc
@@ -8,14 +8,38 @@ using namespace std;
 using namespace std::chrono;
 using namespace engc::tools;
 
-void calculation() {
-    EnginePowerCalculator::plotPowerByRodLength();
+static const int EXIT = 0;
+static const int PLOT_ENGINE_POWER_BY_ROD_LENGTH = 1;
+static const int CALCULATE_OPTIMUM_REAR_SUSPENSION_BARS_LENGTH = 2;
+
+void calculate(int what) {
+    switch (what) {
+        case PLOT_ENGINE_POWER_BY_ROD_LENGTH: EnginePowerCalculator::plotPowerByRodLength();
+            break;
+        case CALCULATE_OPTIMUM_REAR_SUSPENSION_BARS_LENGTH: int a;
+            break;
+        case EXIT:
+            return;
+        default: cout << "Unknown option, exiting" << endl;
+    }
+
+}
+
+void printUsage() {
+    cout << "To plot Engine power by Rod length, enter " << PLOT_ENGINE_POWER_BY_ROD_LENGTH << endl;
+    cout << "To calculate optimum rear suspension rods length, enter " << CALCULATE_OPTIMUM_REAR_SUSPENSION_BARS_LENGTH << endl;
+    cout << "To Cancel, enter " << EXIT << endl;
 }
 
 int main() {
+
+    printUsage();
+
+    int option;
+    cin >> option;
     
     high_resolution_clock::time_point t1 = high_resolution_clock::now();
-    calculation();
+    calculate(option);
     high_resolution_clock::time_point t2 = high_resolution_clock::now();
 
     auto duration = duration_cast<milliseconds>( t2 - t1 ).count();
