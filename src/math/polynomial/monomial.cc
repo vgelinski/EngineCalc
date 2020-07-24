93e00dfcdec00a7e52c0395bcb7a914dc7a1ca05
Valentin Gelinski <gelinski.v@gmail.com>
2020-07-25 00:33:49 +0300

    Implemented Monomial

--- src/math/polynomial/monomial.cc
+++ src/math/polynomial/monomial.cc
@@ -1,5 +1,8 @@
 #include "monomial.h"
 
+#include <numeric>
+#include <cmath>
+
 using namespace std;
 using namespace engc::math;
 
@@ -7,17 +10,32 @@ Monomial::Monomial() : powers(), multiplier(1.0L) {}
 Monomial::~Monomial() {}
 
 fret_t Monomial::value(const fparams_t& params) const {
-    return 0.0L;
+    return std::accumulate(
+            powers.begin(),
+            powers.end(),
+            multiplier,
+            [this, params](const fret_t& acc, const pair<string, uint_fast8_t>& entry) -> fret_t {
+                return acc * pow(params.at(entry.first), entry.second);
+            }
+    );
 }
 
 fvariables_t Monomial::variables() const {
-    return fvariables_t({});
+    return std::accumulate(
+            powers.begin(),
+            powers.end(),
+            fvariables_t({}),
+            [this](fvariables_t& acc, const pair<string, uint_fast8_t>& entry) -> fvariables_t {
+                acc.insert(entry.first);
+                return acc;
+            }
+    );
 }
 
 void Monomial::setMultiplier(const fret_t& multiplier) {
-
+    this->multiplier = multiplier;
 }
 
 void Monomial::addVariable(const std::string& name, const uint_fast8_t& power) {
-
+    powers[name] = power;
 }
