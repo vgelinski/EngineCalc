9e51859b3dbb38638f2b158f5463b7c1fe4faa36
Valentin Gelinski <gelinski.v@gmail.com>
2020-07-30 14:27:07 +0300

    Implemented max  function

--- Makefile
+++ Makefile
@@ -25,6 +25,7 @@ src/math/function.cc\
 src/math/identity.cc\
 src/math/implicit_function.cc\
 src/math/integral.cc\
+src/math/max_function.cc\
 src/math/polynomial/lagrange_polynom_builder.cc\
 src/math/polynomial/monomial.cc\
 src/math/polynomial/polynomial.cc\
---
+++ src/math/max_function.h
@@ -0,0 +1,24 @@
+#ifndef ENGINECALC_MAX_FUNCTION_H
+#define ENGINECALC_MAX_FUNCTION_H
+
+#include "function.h"
+
+namespace engc::math {
+class MaxFunction : public Function {
+private:
+    std::unordered_set<std::shared_ptr<Function>> functions;
+
+protected:
+    virtual fret_t value(const fparams_t& params) const override;
+
+public:
+    MaxFunction();
+    virtual ~MaxFunction();
+
+    virtual fvariables_t variables() const override;
+    virtual void addFunction(const std::shared_ptr<Function>& f);
+};
+};
+
+
+#endif //ENGINECALC_MAX_FUNCTION_H
