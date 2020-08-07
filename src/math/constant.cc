9c69f15d33ca3c7f940c7a4a5ba13deb4c6aeb1a
Valentin Gelinski <gelinski.v@gmail.com>
2020-08-07 20:00:31 +0300

    Implemented constant toString

--- src/math/constant.cc
+++ src/math/constant.cc
@@ -11,6 +11,12 @@ fret_t Constant::value(const fparams_t& params) const {
     return val;
 };
 
+string Constant::toStringImpl() const {
+    char buffer[1024];
+    sprintf(buffer, "%Lg", val);
+    return string(buffer);
+};
+
 fvariables_t Constant::variables() const {
     return fvariables_t();
 }
--- src/math/constant.h
+++ src/math/constant.h
@@ -19,6 +19,7 @@ private:
 protected:
 
     virtual fret_t value(const fparams_t& params) const override;
+    virtual std::string toStringImpl() const override;
 
 public:
 
