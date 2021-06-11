7af679ddb538453e97699a6dac263bbef2cc6ce4
Valentin Gelinski <gelinski.v@gmail.com>
2021-06-11 19:37:52 +0300

    Add logs to coverage script

--- coverage.sh
+++ coverage.sh
@@ -7,5 +7,6 @@ gcov -b -c tobj/test/test_runner.cc
 mkdir -p $COV_DIR
 find tobj/|grep gcno|while read FILE; do cp $FILE coverage/; done
 find tobj/|grep gcda|while read FILE; do cp $FILE coverage/; done
+pwd
 lcov --directory $COV_DIR --capture --output-file $LCOV_OUT
 genhtml $LCOV_OUT --output-directory $COV_DIR
